f8a2c5343e85421dfbac57664aaf8642c6aee1ef
hotc007 <934157466@qq.com>
2018-07-04 15:48:54 +0800

    更新merkle tree的算法和文档

--- libraries/chain/protocol/block.cpp
+++ libraries/chain/protocol/block.cpp
@@ -39,9 +39,9 @@ namespace hotc { namespace chain {
 
    block_id_type signed_block_header::id()const
    {
-      auto tmp = fc::sha224::hash( *this );
+      auto tmp = fc::sha224::hash(*this);
       tmp._hash[0] = fc::endian_reverse_u32(block_num()); // store the block num in the ID, 160 bits is plenty for the hash
-      static_assert( sizeof(tmp._hash[0]) == 4, "should be 4 bytes" );
+      static_assert(sizeof(tmp._hash[0]) == 4, "should be 4 bytes");
       block_id_type result;
       memcpy(result._hash, tmp._hash, std::min(sizeof(result), sizeof(tmp)));
       return result;
@@ -49,44 +49,72 @@ namespace hotc { namespace chain {
 
    fc::ecc::public_key signed_block_header::signee()const
    {
-      return fc::ecc::public_key( producer_signature, digest(), true/*enforce canonical*/ );
+      return fc::ecc::public_key(producer_signature, digest(), true/*enforce canonical*/);
    }
 
-   void signed_block_header::sign( const fc::ecc::private_key& signer )
+   void signed_block_header::sign(const fc::ecc::private_key& signer)
    {
-      producer_signature = signer.sign_compact( digest() );
+      producer_signature = signer.sign_compact(digest());
    }
 
-   bool signed_block_header::validate_signee( const fc::ecc::public_key& expected_signee )const
+   bool signed_block_header::validate_signee(const fc::ecc::public_key& expected_signee)const
    {
       return signee() == expected_signee;
    }
 
+   digest_type merkle(vector<digest_type> ids) {
+      while (ids.size() > 1) {
+         if (ids.size() % 2)
+            ids.push_back(ids.back());
+         for (int i = 0; i < ids.size() / 2; ++i)
+            ids[i/2] = digest_type::hash(std::make_pair(ids[i], ids[i+1]));
+         ids.resize(ids.size() / 2);
+      }
+
+      return ids.front();
+   }
+
    checksum_type signed_block::calculate_merkle_root()const
    {
-      if( transactions.size() == 0 )
+      if(cycles.empty())
          return checksum_type();
 
       vector<digest_type> ids;
-      ids.resize( transactions.size() );
-      for( uint32_t i = 0; i < transactions.size(); ++i )
-         ids[i] = transactions[i].merkle_digest();
+      for (const auto& cycle : cycles)
+         for (const auto& thread : cycle)
+            ids.emplace_back(thread.merkle_digest());
 
-      vector<digest_type>::size_type current_number_of_hashes = ids.size();
-      while( current_number_of_hashes > 1 )
-      {
-         // hash ID's in pairs
-         uint32_t i_max = current_number_of_hashes - (current_number_of_hashes&1);
-         uint32_t k = 0;
+/**
+ *  Suggest moving thread::merkle_digest code to return vector of ids which get added to ids above and then calculating root over all
+ */
+#warning TODO  The merkle root needs to be over all transactions, but this is currently hashing all threads merkle roots which will make proofs O(N) rather than O( LOG(N) )
 
-         for( uint32_t i = 0; i < i_max; i += 2 )
-            ids[k++] = digest_type::hash( std::make_pair( ids[i], ids[i+1] ) );
+      /**
+       *  This may require passing 
+       */
+#warning TODO  Add global incremental block header merkle   https://github.com/HOTCIO/hotc/issues/8
 
-         if( current_number_of_hashes&1 )
-            ids[k++] = ids[i_max];
-         current_number_of_hashes = k;
-      }
-      return checksum_type::hash( ids[0] );
+      return checksum_type::hash(merkle(ids));
+   }
+
+   digest_type thread::merkle_digest() const {
+      vector<digest_type> ids = generated_input;
+      ids.reserve( ids.size() + user_input.size() + output_transactions.size() );
+
+      for( const auto& trx : user_input )
+         ids.push_back( trx.digest() );
+
+      /**
+       *  When generating the merkle hash of an output transaction we hash it
+       *  a second time. This is because the transaction has not been confirmed as
+       *  "valid and applied" just "produced".  Later, when this transaction is included
+       *  as part of "generated input" its ID will be used without the extra hash.
+       */
+      for( const auto& trx : output_transactions )
+         ids.push_back( digest_type::hash(trx.digest()) );
+
+
+      return merkle(ids);
    }
 
 } }
