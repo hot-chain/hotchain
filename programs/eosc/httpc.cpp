45f57c2ef2fa6736eb9842a0ab0bccc275870a95
hotchain001 <hotc001@163.com>
2018-09-11 17:46:50 +0800

    添加客户端与命令行交互

--- programs/eosc/httpc.cpp
+++ programs/eosc/httpc.cpp
@@ -1,119 +1,320 @@
-//
-// sync_client.cpp
-// ~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+/*
+ * Copyright (c) 2017, Respective Authors.
+ *
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#pragma once
+#include <hotc/chain/global_property_object.hpp>
+#include <hotc/chain/account_object.hpp>
+#include <hotc/chain/fork_database.hpp>
+#include <hotc/chain/block_log.hpp>
 
-#include <iostream>
-#include <istream>
-#include <ostream>
-#include <string>
-#include <boost/asio.hpp>
-#include <fc/variant.hpp>
-#include <fc/io/json.hpp>
-#include <fc/exception/exception.hpp>
+#include <chainbase/chainbase.hpp>
+#include <fc/scoped_exit.hpp>
 
-using boost::asio::ip::tcp;
+#include <boost/signals2/signal.hpp>
 
-fc::variant call( const std::string& server, uint16_t port, 
-                  const std::string& path,
-                  const fc::variant& postdata ) 
-{ try {
+#include <hotc/chain/protocol.hpp>
+#include <hotc/chain/message_handling_contexts.hpp>
+#include <hotc/chain/chain_initializer_interface.hpp>
+#include <hotc/chain/chain_administration_interface.hpp>
 
-    std::string postjson;
-    if( !postdata.is_null() )
-       postjson = fc::json::to_string( postdata );
+#include <fc/log/logger.hpp>
 
-    boost::asio::io_service io_service;
+#include <map>
 
-    // Get a list of endpoints corresponding to the server name.
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query query(server, std::to_string(port) );
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-    tcp::resolver::iterator end;
+namespace hotc { namespace chain {
+   using database = chainbase::database;
+   using boost::signals2::signal;
 
-    while( endpoint_iterator != end ) {
-       // Try each endpoint until we successfully establish a connection.
-       tcp::socket socket(io_service);
-       try {
-          boost::asio::connect(socket, endpoint_iterator);
-          endpoint_iterator = end;
-       } catch( std::exception& e ) {
-          ++endpoint_iterator;
-          if( endpoint_iterator != end )
-             continue;
-          else throw;
-       }
+   /**
+    *   @class database
+    *   @brief tracks the blockchain state in an extensible manner
+    */
+   class chain_controller {
+      public:
+         chain_controller(database& database, fork_database& fork_db, block_log& blocklog,
+                          chain_initializer_interface& starter, unique_ptr<chain_administration_interface> admin);
+         chain_controller(chain_controller&&) = default;
+         ~chain_controller();
 
-       // Form the request. We specify the "Connection: close" header so that the
-       // server will close the socket after transmitting the response. This will
-       // allow us to treat all data up until the EOF as the content.
-       boost::asio::streambuf request;
-       std::ostream request_stream(&request);
-       request_stream << "POST " << path << " HTTP/1.0\r\n";
-       request_stream << "Host: " << server << "\r\n";
-       request_stream << "content-length: " << postjson.size() << "\r\n";
-       request_stream << "Accept: */*\r\n";
-       request_stream << "Connection: close\r\n\r\n";
-       request_stream << postjson;
+         /**
+          *  This signal is emitted after all operations and virtual operation for a
+          *  block have been applied but before the get_applied_operations() are cleared.
+          *
+          *  You may not yield from this callback because the blockchain is holding
+          *  the write lock and may be in an "inconstant state" until after it is
+          *  released.
+          */
+         signal<void(const signed_block&)> applied_block;
 
-       // Send the request.
-       boost::asio::write(socket, request);
+         /**
+          * This signal is emitted any time a new transaction is added to the pending
+          * block state.
+          */
+         signal<void(const SignedTransaction&)> on_pending_transaction;
 
-       // Read the response status line. The response streambuf will automatically
-       // grow to accommodate the entire line. The growth may be limited by passing
-       // a maximum size to the streambuf constructor.
-       boost::asio::streambuf response;
-       boost::asio::read_until(socket, response, "\r\n");
 
-       // Check that response is OK.
-       std::istream response_stream(&response);
-       std::string http_version;
-       response_stream >> http_version;
-       unsigned int status_code;
-       response_stream >> status_code;
-       std::string status_message;
-       std::getline(response_stream, status_message);
-       FC_ASSERT( !(!response_stream || http_version.substr(0, 5) != "HTTP/"), "Invalid Response" );
+         /**
+          *  The controller can override any script endpoint with native code.
+          */
+         ///@{
+         void set_apply_handler( const AccountName& contract, const AccountName& scope, const ActionName& action, apply_handler v );
+         //@}
 
-       // Read the response headers, which are terminated by a blank line.
-       boost::asio::read_until(socket, response, "\r\n\r\n");
+         enum validation_steps
+         {
+            skip_nothing                = 0,
+            skip_producer_signature     = 1 << 0,  ///< used while reindexing
+            skip_transaction_signatures = 1 << 1,  ///< used by non-producer nodes
+            skip_transaction_dupe_check = 1 << 2,  ///< used while reindexing
+            skip_fork_db                = 1 << 3,  ///< used while reindexing
+            skip_block_size_check       = 1 << 4,  ///< used when applying locally generated transactions
+            skip_tapos_check            = 1 << 5,  ///< used while reindexing -- note this skips expiration check as well
+            skip_authority_check        = 1 << 6,  ///< used while reindexing -- disables any checking of authority on transactions
+            skip_merkle_check           = 1 << 7,  ///< used while reindexing
+            skip_assert_evaluation      = 1 << 8,  ///< used while reindexing
+            skip_undo_history_check     = 1 << 9,  ///< used while reindexing
+            skip_producer_schedule_check= 1 << 10,  ///< used while reindexing
+            skip_validate               = 1 << 11, ///< used prior to checkpoint, skips validate() call on transaction
+            skip_scope_check            = 1 << 12  ///< used to skip checks for proper scope
+         };
 
-       // Process the response headers.
-       std::string header;
-       while (std::getline(response_stream, header) && header != "\r")
-       {
-//         std::cout << header << "\n";
-       }
- //      std::cout << "\n";
+         /**
+          *  @return true if the block is in our fork DB or saved to disk as
+          *  part of the official chain, otherwise return false
+          */
+         bool                       is_known_block( const block_id_type& id )const;
+         bool                       is_known_transaction( const transaction_id_type& id )const;
+         block_id_type              get_block_id_for_num( uint32_t block_num )const;
+         optional<signed_block>     fetch_block_by_id( const block_id_type& id )const;
+         optional<signed_block>     fetch_block_by_number( uint32_t num )const;
+         const SignedTransaction&   get_recent_transaction( const transaction_id_type& trx_id )const;
+         std::vector<block_id_type> get_block_ids_on_fork(block_id_type head_of_fork)const;
 
-       std::stringstream re;
-       // Write whatever content we already have to output.
-       if (response.size() > 0)
-      //   std::cout << &response;
-         re << &response;
+         /**
+          *  This method will convert a variant to a SignedTransaction using a contract's ABI to
+          *  interpret the message types.
+          */
+         ProcessedTransaction transaction_from_variant( const fc::variant& v )const;
 
-       // Read until EOF, writing data to output as we go.
-       boost::system::error_code error;
-       while (boost::asio::read(socket, response,
-             boost::asio::transfer_at_least(1), error))
-          re << &response;
+         /**
+          * This method will convert a signed transaction into a human-friendly variant that can be
+          * converted to JSON.  
+          */
+         fc::variant       transaction_to_variant( const ProcessedTransaction& trx )const;
 
-       if (error != boost::asio::error::eof)
-         throw boost::system::system_error(error);
+         /**
+          *  Usees the ABI for code::type to convert a JSON object (variant) into hex
+          */
+         vector<char>       message_to_binary( Name code, Name type, const fc::variant& obj )const;
 
-     //  std::cout << re.str() <<"\n";
-       if( status_code == 200 ) {
-          return fc::json::from_string(re.str());
-       }
 
-       FC_ASSERT( status_code == 200, "Error\n: ${msg}\n", ("msg", re.str()) );
-    }
+         /**
+          *  Calculate the percent of block production slots that were missed in the
+          *  past 128 blocks, not including the current block.
+          */
+         uint32_t producer_participation_rate()const;
 
-    FC_ASSERT( !"unable to connect" );
-  } FC_CAPTURE_AND_RETHROW( (server)(port)(path)(postdata) ) 
-}
+         void                                   add_checkpoints(const flat_map<uint32_t,block_id_type>& checkpts);
+         const flat_map<uint32_t,block_id_type> get_checkpoints()const { return _checkpoints; }
+         bool before_last_checkpoint()const;
+
+         bool push_block( const signed_block& b, uint32_t skip = skip_nothing );
+
+
+         ProcessedTransaction push_transaction( const SignedTransaction& trx, uint32_t skip = skip_nothing );
+         ProcessedTransaction _push_transaction( const SignedTransaction& trx );
+
+
+         bool _push_block( const signed_block& b );
+
+         signed_block generate_block(
+            fc::time_point_sec when,
+            const AccountName& producer,
+            const fc::ecc::private_key& block_signing_private_key,
+            uint32_t skip
+            );
+         signed_block _generate_block(
+            fc::time_point_sec when,
+            const AccountName& producer,
+            const fc::ecc::private_key& block_signing_private_key
+            );
+
+
+         template<typename Function>
+         auto with_skip_flags( uint64_t flags, Function&& f ) -> decltype((*((Function*)nullptr))()) 
+         {
+            auto old_flags = _skip_flags;
+            auto on_exit   = fc::make_scoped_exit( [&](){ _skip_flags = old_flags; } );
+            _skip_flags = flags;
+            return f();
+         }
+
+         template<typename Function>
+         auto without_pending_transactions( Function&& f ) -> decltype((*((Function*)nullptr))()) 
+         {
+            auto old_pending = std::move( _pending_transactions );
+            _pending_tx_session.reset();
+            auto on_exit = fc::make_scoped_exit( [&](){ 
+               for( const auto& t : old_pending ) {
+                  try {
+                     if (!is_known_transaction(t.id()))
+                        push_transaction( t );
+                  } catch ( ... ){}
+               }
+            });
+            return f();
+         }
+
+         void pop_block();
+         void clear_pending();
+
+         /**
+          * @brief Get the producer scheduled for block production in a slot.
+          *
+          * slot_num always corresponds to a time in the future.
+          *
+          * If slot_num == 1, returns the next scheduled producer.
+          * If slot_num == 2, returns the next scheduled producer after
+          * 1 block gap.
+          *
+          * Use the get_slot_time() and get_slot_at_time() functions
+          * to convert between slot_num and timestamp.
+          *
+          * Passing slot_num == 0 returns HOTC_NULL_PRODUCER
+          */
+         AccountName get_scheduled_producer(uint32_t slot_num)const;
+
+         /**
+          * Get the time at which the given slot occurs.
+          *
+          * If slot_num == 0, return time_point_sec().
+          *
+          * If slot_num == N for N > 0, return the Nth next
+          * block-interval-aligned time greater than head_block_time().
+          */
+         fc::time_point_sec get_slot_time(uint32_t slot_num)const;
+
+         /**
+          * Get the last slot which occurs AT or BEFORE the given time.
+          *
+          * The return value is the greatest value N such that
+          * get_slot_time( N ) <= when.
+          *
+          * If no such N exists, return 0.
+          */
+         uint32_t get_slot_at_time(fc::time_point_sec when)const;
+
+         const global_property_object&          get_global_properties()const;
+         const dynamic_global_property_object&  get_dynamic_global_properties()const;
+         const producer_object&                 get_producer(const AccountName& ownerName)const;
+
+         time_point_sec   head_block_time()const;
+         uint32_t         head_block_num()const;
+         block_id_type    head_block_id()const;
+         AccountName      head_block_producer()const;
+
+         uint32_t block_interval()const { return config::BlockIntervalSeconds; }
+
+         uint32_t last_irreversible_block_num() const;
+
+ //  protected:
+         const chainbase::database& get_database() const { return _db; }
+         chainbase::database& get_mutable_database() { return _db; }
+         
+         bool should_check_scope()const                      { return !(_skip_flags&skip_scope_check);            }
+   private:
+
+         /// Reset the object graph in-memory
+         void initialize_indexes();
+         void initialize_chain(chain_initializer_interface& starter);
+
+         void replay();
+
+         void apply_block(const signed_block& next_block, uint32_t skip = skip_nothing);
+         void _apply_block(const signed_block& next_block);
+
+
+         ProcessedTransaction apply_transaction(const SignedTransaction& trx, uint32_t skip = skip_nothing);
+         ProcessedTransaction _apply_transaction(const SignedTransaction& trx);
+         ProcessedTransaction process_transaction( const SignedTransaction& trx );
+
+         void require_account(const AccountName& name) const;
+
+         /**
+          * This method validates transactions without adding it to the pending state.
+          * @return true if the transaction would validate
+          */
+         void validate_transaction(const SignedTransaction& trx)const;
+         /// Validate transaction helpers @{
+         void validate_uniqueness(const SignedTransaction& trx)const;
+         void validate_tapos(const SignedTransaction& trx)const;
+         void validate_referenced_accounts(const SignedTransaction& trx)const;
+         void validate_expiration(const SignedTransaction& trx) const;
+         void validate_scope(const SignedTransaction& trx) const;
+         void validate_authority(const SignedTransaction& trx )const;
+         /// @}
+
+         void process_message(const ProcessedTransaction& trx, AccountName code, const Message& message, MessageOutput& output);
+         void apply_message(apply_context& c);
+
+         bool should_check_for_duplicate_transactions()const { return !(_skip_flags&skip_transaction_dupe_check); }
+         bool should_check_tapos()const                      { return !(_skip_flags&skip_tapos_check);            }
+
+         ///Steps involved in applying a new block
+         ///@{
+         const producer_object& validate_block_header(uint32_t skip, const signed_block& next_block)const;
+         const producer_object& _validate_block_header(const signed_block& next_block)const;
+         void create_block_summary(const signed_block& next_block);
+
+         void update_global_properties(const signed_block& b);
+         void update_global_dynamic_data(const signed_block& b);
+         void update_signing_producer(const producer_object& signing_producer, const signed_block& new_block);
+         void update_last_irreversible_block();
+         void clear_expired_transactions();
+         /// @}
+
+         void spinup_db();
+         void spinup_fork_db();
+
+         ProducerRound calculate_next_round(const signed_block& next_block);
+
+         database&                        _db;
+         fork_database&                   _fork_db;
+         block_log&                       _block_log;
+
+         unique_ptr<chain_administration_interface> _admin;
+
+         optional<database::session>      _pending_tx_session;
+         deque<SignedTransaction>         _pending_transactions;
+
+         bool                             _pushing  = false;
+         uint64_t                         _skip_flags = 0;
+
+         flat_map<uint32_t,block_id_type> _checkpoints;
+
+         typedef pair<AccountName,types::Name> handler_key;
+
+         map< AccountName, map<handler_key, apply_handler> >                   apply_handlers;
+   };
+
+} }
--- programs/eosc/main.cpp
+++ programs/eosc/main.cpp
@@ -1,57 +1,171 @@
-#include <string>
-#include <vector>
-#include <boost/asio.hpp>
-#include <iostream>
-#include <fc/variant.hpp>
-#include <fc/io/json.hpp>
-#include <fc/exception/exception.hpp>
+#include <omo/types/AbiSerializer.hpp>
+#include <fc/io/raw.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 
-using namespace std;
+namespace omo { namespace types {
 
-
-fc::variant call( const std::string& server, uint16_t port, 
-                  const std::string& path,
-                  const fc::variant& postdata = fc::variant() );
-
-/**
- *   Usage:
- *
- *   eocs create wallet walletname  ***PASS1*** ***PASS2*** 
- *   hotcc unlock walletname  ***PASSWORD*** 
- *   hotcc wallets -> prints list of wallets with * next to currently unlocked
- *   hotcc keys -> prints list of private keys
- *   hotcc importkey privatekey -> loads keys
- *   hotcc accounts -> prints list of accounts that reference key
- *   hotcc lock
- *   hotcc do contract action { from to amount }
- *   hotcc transfer from to amount memo  => aliaze for hotcc 
- *   hotcc create account creator 
- */
-int main( int argc, char** argv ) {
-   try {
-   vector<string> args(argc);
-   for( uint32_t i = 0; i < argc; ++i ) {
-      args[i] = string(argv[i]);
+   AbiSerializer::AbiSerializer( const Abi& abi ) {
+      setAbi(abi);
    }
-   const auto& command = args[1];
-   if( command == "info" ) {
-      std::cout << fc::json::to_pretty_string( call( "localhost", 8888, "/v1/chain/get_info" ) );
-   }
-   else if( command == "block" ) {
-      FC_ASSERT( args.size() == 3 );
-      std::cout << fc::json::to_pretty_string( call( "localhost", 8888, "/v1/chain/get_block", fc::mutable_variant_object( "block_num_or_id", args[2]) ) );
-   }
-   if( command == "unlock" ) {
 
-   } else if( command == "lock" ) {
+   void AbiSerializer::setAbi( const Abi& abi ) {
+      typedefs.clear();
+      structs.clear();
+      actions.clear();
+      tables.clear();
 
-   } else if( command == "do" ) {
+      for( const auto& td : abi.types )
+         typedefs[td.newTypeName] = td.type;
 
+      for( const auto& st : abi.structs )
+         structs[st.name] = st;
+      
+      for( const auto& a : abi.actions )
+         actions[a.action] = a.type;
+      for( const auto& t : abi.tables )
+         tables[t.table] = t.type;
+
+      /**
+       *  The ABI vector may contain duplicates which would make it
+       *  an invalid ABI
+       */
+      FC_ASSERT( typedefs.size() == abi.types.size() );
+      FC_ASSERT( structs.size() == abi.structs.size() );
+      FC_ASSERT( actions.size() == abi.actions.size() );
+      FC_ASSERT( tables.size() == abi.tables.size() );
    }
-   } catch ( const fc::exception& e ) {
-      std::cerr << e.to_detail_string() <<"\n";
-   }
-   std::cout << "\n";
 
-   return 0;
-}
+   bool AbiSerializer::isType( const TypeName& type )const {
+      static const std::set<TypeName> native = {"Name","UInt64"};
+      if( native.find(type) != native.end() ) return true;
+      if( typedefs.find(type) != typedefs.end() ) return isType( typedefs.find(type)->second );
+      if( structs.find(type) != structs.end() ) return true;
+      return false;
+   }
+
+   const Struct& AbiSerializer::getStruct( const TypeName& type )const {
+      if( typedefs.find(type) != typedefs.end() ) return getStruct( typedefs.find(type)->second );
+      auto itr = structs.find(type);
+      FC_ASSERT( itr != structs.end(), "Unknown struct ${type}", ("type",type) );
+      return itr->second;
+   }
+
+   void AbiSerializer::validate()const {
+      for( const auto& t : typedefs ) { try {
+         FC_ASSERT( isType( t.second ), "", ("type",t.second) );
+      } FC_CAPTURE_AND_RETHROW( (t) ) }
+      for( const auto& s : structs ) { try {
+         if( s.second.base != TypeName() )
+            FC_ASSERT( isType( s.second.base ) );
+         for( const auto& field : s.second.fields ) { try {
+            FC_ASSERT( isType( field.type ) );
+         } FC_CAPTURE_AND_RETHROW( (field) ) }
+      } FC_CAPTURE_AND_RETHROW( (s) ) }
+      for( const auto& a : actions ) { try {
+        FC_ASSERT( isType( a.second ), "", ("type",a.second) );
+      } FC_CAPTURE_AND_RETHROW( (a)  ) }
+
+      for( const auto& t : tables ) { try {
+        FC_ASSERT( isType( t.second ), "", ("type",t.second) );
+      } FC_CAPTURE_AND_RETHROW( (t)  ) }
+   }
+
+   TypeName AbiSerializer::resolveType( const TypeName& type )const  {
+      auto itr = typedefs.find(type);
+      if( itr != typedefs.end() )
+         return resolveType(itr->second);
+      return type;
+   }
+
+   void AbiSerializer::binaryToVariant(const TypeName& type, fc::datastream<const char*>& stream, fc::mutable_variant_object& obj )const {
+      const auto& st = getStruct( type );
+      if( st.base != TypeName() ) {
+         binaryToVariant( resolveType(st.base), stream, obj );
+         return;
+      }
+      for( const auto& field : st.fields ) {
+         obj( field.name, binaryToVariant( resolveType(field.type), stream ) );
+      }
+   }
+   fc::variant AbiSerializer::binaryToVariant(const TypeName& type, fc::datastream<const char*>& stream )const
+   {
+      TypeName rtype = resolveType( type );
+      if( rtype == "Name" ) {
+        Name temp;
+        fc::raw::unpack( stream, temp );
+        return fc::variant(temp);
+      }
+      else if( rtype == "UInt64" ) {
+        uint64_t temp;
+        fc::raw::unpack( stream, temp );
+        return fc::variant(temp);
+      }
+      fc::mutable_variant_object mvo;
+      binaryToVariant( rtype, stream, mvo );
+      return fc::variant( std::move(mvo) );
+   }
+
+   fc::variant AbiSerializer::binaryToVariant(const TypeName& type, const Bytes& binary)const{
+      fc::datastream<const char*> ds( binary.data(), binary.size() );
+      return binaryToVariant( type, ds );
+   }
+
+   void AbiSerializer::variantToBinary(const TypeName& type, const fc::variant& var, fc::datastream<char*>& ds )const
+   {
+      auto rtype = resolveType(type);
+
+      if( rtype == "Name" ) {
+         fc::raw::pack( ds, var.as<Name>() );
+         return;
+      }
+      else if( rtype == "UInt64" ) {
+         fc::raw::pack( ds, var.as<uint64_t>() );
+         return;
+      }
+      else if( rtype == "String" ) {
+         fc::raw::pack( ds, var.as<String>() );
+         return;
+      } else {
+         const auto& st = getStruct( rtype );
+         const auto& vo = var.get_object();
+
+         if( st.base != TypeName() ) {
+            variantToBinary( resolveType( st.base ), var, ds );
+         }
+         for( const auto& field : st.fields ) {
+            if( vo.contains( String(field.name).c_str() ) ) {
+               variantToBinary( field.type, vo[field.name], ds );
+            }
+            else {
+               /// TODO: default construct field and write it out
+               FC_ASSERT( !"missing field in variant object", "Missing '${f}' in variant object", ("f",field.name) );
+            }
+         }
+      }
+   }
+
+
+
+   Bytes       AbiSerializer::variantToBinary(const TypeName& type, const fc::variant& var)const {
+      if( !isType(type) )
+         return var.as<Bytes>();
+
+      Bytes temp( 1024*1024 );
+      fc::datastream<char*> ds(temp.data(), temp.size() );
+      variantToBinary( type, var, ds );
+      temp.resize(ds.tellp());
+      return temp;
+   }
+
+   TypeName AbiSerializer::getActionType( Name action )const {
+      auto itr = actions.find(action);
+      if( itr != actions.end() ) return itr->second;
+      return TypeName();
+   }
+   TypeName AbiSerializer::getTableType( Name action )const {
+      auto itr = tables.find(action);
+      if( itr != tables.end() ) return itr->second;
+      return TypeName();
+   }
+
+} }
